c8d32f6552085dd5d2f258cda6a3eaedef7128e9
I-Tai Lin <smes93434@gmail.com>
2023-12-12 14:51:03 -1000

    add the bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
+import BasicExample from './BasicExample';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicExample />
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
+      <Form />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,9 @@
+const from = () => {
+    return ( 
+        <div className="form">
+            <h1>Test</h1>
+        </div>
+     );
+}
+ 
+export default from;
