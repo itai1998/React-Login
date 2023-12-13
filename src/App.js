61e0e0922b64625e7b941f0501f1f91b9472fd98
I-Tai Lin <smes93434@gmail.com>
2023-12-13 08:33:51 -1000

    mkae the login page pretter

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Form from './Form';
 import BasicExample from './BasicExample';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const backgroundImageStyle = {
+  backgroundImage: 'url("https://brightspotcdn.byu.edu/dc/d7/c22c23614dd5870f2fcbbd473f06/jango-graduation-emmalee-169.jpg")',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+  minHeight: '100vh', 
+};
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={backgroundImageStyle}>
       <BasicExample />
       <div className="content">
-        <h1>Please Log in</h1>
+        <h1>Please Login</h1>
       </div>
       <Form />
     </div>
