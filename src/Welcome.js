c2dd9a6de1cf11794967748e8e7e42ccc69bb1b4
I-Tai Lin <smes93434@gmail.com>
2023-12-14 15:28:11 -1000

    create JSON local server and retrive the data

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import useFetch from './useFetch';
+
 const Welcome = () => {
-    return ( 
-        <div className="welcome-container">
-            <h1>Welcome</h1>
-        </div>
-     );
-}
- 
+  const { data, error } = useFetch('http://localhost:8000/users');
+
+  if (error) {
+    return <div>Error fetching data: {error.message}</div>;
+  }
+
+  return (
+    <div className="welcome-container">
+      <h1>Welcome</h1>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </div>
+  );
+};
+
 export default Welcome;
