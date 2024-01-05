a3b13637ba6cf9670adce9f42ac8c822807755f0
I-Tai Lin <smes93434@gmail.com>
2024-01-05 13:59:42 -1000

    make the product to be dynamic functionaility

--- src/Products.js
+++ src/Products.js
@@ -1,11 +1,15 @@
-const Products = () => {
+const Products = ({id,title,image,rating,price}) => {
     return ( 
         <div className="d-flex flex-column">
-            <h2>Title of Product</h2>
-            <strong>$12</strong>
-            <p>⭐⭐⭐</p>
+            <h4>{title}</h4>
+            <strong>${price}</strong>
+            <p>
+                {Array(rating).fill().map((_,i) =>(
+                    <span>⭐</span>
+                ))}
+            </p>
             <div className="d-flex flex-column align-items-center">
-                <img src="/Images/mac-m3-pro.jpg" alt="mac-m3-pro" className="w-50 mt-1"/>
+                <img src={`/Images/${image}`} alt="mac-m3-pro" className="w-50 mt-1"/>
                 <button className="btn btn-warning w-suto mt-3">Add Product</button>
             </div>
         </div>
--- src/Welcome.js
+++ src/Welcome.js
@@ -17,26 +17,62 @@ const Welcome = () => {
       <div className="products bg-light">
         <div className="p-3 d-flex">
           <div className="w-50 p-2 bg-white m-2">
-            <Products />
+            <Products
+            id={1}
+            title = {"Mac Book M3 Pro"}
+            image = {"mac-m3-pro.jpg"}
+            rating = {4}
+            price = {1800}
+            />
           </div>
           <div className="w-50 p-2 bg-white m-2">
-            <Products />
+            <Products
+            id={2}
+            title = {"Water Bottle"}
+            image = {"Water-Bottle.jpg"}
+            rating = {4}
+            price = {10}
+            />
           </div>
         </div>
         <div className="p-3 d-flex">
           <div className="p-2 bg-white m-2">
-            <Products />
+            <Products
+            id={3}
+            title = {"Apple Watch"}
+            image = {"Apple-Watch.png"}
+            rating = {3}
+            price = {890}
+            />
           </div>
           <div className="p-2 bg-white m-2">
-            <Products />
+            <Products
+            id={4}
+            title = {"Gaming Chair"}
+            image = {"Gaming-Chair.webp"}
+            rating = {5}
+            price = {120}
+            />
           </div>
           <div className="p-2 bg-white m-2">
-            <Products />
+            <Products
+            id={5}
+            title = {"Headphone"}
+            image = {"Headphone.webp"}
+            rating = {3}
+            price = {38}
+            />
           </div>
         </div>
         <div className="p-3">
           <div className="p-2 bg-white m-2 d-flex justify-content-center">
-            <Products />
+            <Products
+            id={6}
+            title = {"T-Shirt"}
+            image = {"T-Shirt.jpg"}
+            rating = {1}
+            price = {20}
+            />
           </div>
         </div>
       </div>
